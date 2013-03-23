1473d9d6f2872b6a21a310cfcaf852608dcaa8f9
Lubomir Petera <xpeteral@centrum.cz>
2013-03-23 16:19:52 +0100

    First test commit of test class

---
+++ kc/src/HelloWorldMain.java
@@ -0,0 +1,26 @@
+/**
+ * Main class for test.
+ * 
+ * @author Lubomir Petera
+ * @version 0.1
+ * @since 2013-03-23
+ *
+ */
+public class HelloWorldMain {
+
+	/**
+	 * No instance allowed.
+	 */
+	private HelloWorldMain() {
+		// NOOP
+	}
+
+	/**
+	 * @param args command line arguments. Not in use.
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello kivCommander.");
+
+	}
+
+}
