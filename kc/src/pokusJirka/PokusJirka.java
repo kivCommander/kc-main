9c1ddfd7322041a3433d737779e1efd2565be2f7
reghled <reghled@centrum.com>
2013-03-28 22:15:17 +0100

    test commit - Jirka

---
+++ kc/src/pokusJirka/PokusJirka.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package pokusJirka;
+
+/**
+ * @author Jiøí
+ *
+ */
+public class PokusJirka {
+
+	/**
+	 * prostì to nedìlá nic, jen zkouška posílání na git... kdo umí, a smae :-)
+	 */
+	public PokusJirka() {
+		System.out.println("Hello famous kivCommander.Jirka.");
+	}
+
+}
