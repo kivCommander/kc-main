8483463dd515f02fa9ebeb06889b8628bef9a8b3
michalriha <michal.riha@hotmail.com>
2013-05-03 10:55:02 +0200

    removed unnecessary files array copying and manual event firing

--- workspace/Core/src/cz/zcu/kiv/kc/shell/FileListModel.java
+++ workspace/Core/src/cz/zcu/kiv/kc/shell/FileListModel.java
@@ -5,31 +5,67 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 import javax.swing.AbstractListModel;
-import javax.swing.event.ListDataEvent;
-import javax.swing.event.ListDataListener;
+//import javax.swing.event.ListDataEvent;
+//import javax.swing.event.ListDataListener;
 
 public class FileListModel extends AbstractListModel<File> {
 
 	private static final long serialVersionUID = 1616095720949603826L;
 
 	private String dirPath = "/";
+	
 	private File[] files = new File[0];
 
+	private File parentDir = null;
+	
+	private Comparator<File> filenameComparator = new Comparator<File>() {
+		@Override
+		public int compare(File arg0, File arg1) {
+			int ret;
+			if (arg0.isDirectory() && !arg1.isDirectory()) {
+				ret = -1;
+			} else if (!arg0.isDirectory() && arg1.isDirectory()) {
+				ret = 1;
+			} else if (arg0.isDirectory() && arg1.isDirectory()) {
+				ret = arg0.getName()
+						.compareToIgnoreCase(arg1.getName());
+			} else {
+				ret = arg0.getName()
+						.compareToIgnoreCase(arg1.getName());
+			}
+			return ret;
+		}
+	};
+	
 	@Override
 	public File getElementAt(int arg0) {
-		return arg0 >=files.length? null: files[arg0];
+		if (this.parentDir != null && arg0 == 0)
+		{
+			return new FirstFile(this.parentDir.getAbsolutePath());
+		}
+		return arg0 >= files.length ? null: files[arg0];
 	}
 
 	@Override
 	public int getSize() {
-		return files.length;
+		return files.length + (this.parentDir != null ? 1 : 0);
 	}
 
 	public void refresh() {
+		int origFilesCount = this.files.length;
 		File dir = new File(dirPath);
-		File[] childFiles = dir.listFiles();
-		int childFilesLength = childFiles == null ? 0 : childFiles.length;
-		String parentFile = dir.getParent();
+		this.parentDir = dir.getParentFile();
+		this.files = dir.listFiles();
+		if (this.files == null)
+		{
+			this.files = new File[0];
+		}
+		
+		Arrays.sort(this.files, this.filenameComparator);
+		
+		this.fireIntervalRemoved(this, 0, origFilesCount);
+		this.fireIntervalAdded(this, 0, this.files.length + (this.parentDir != null ? 1 : 0));
+		/*String parentFile = dir.getParent();
 		if (parentFile != null) {
 			files = new File[childFilesLength + 1];
 			files[0] = new FirstFile(parentFile);
@@ -76,7 +112,7 @@ public class FileListModel extends AbstractListModel<File> {
 		for (ListDataListener listener : listenerList
 				.getListeners(ListDataListener.class)) {
 			listener.contentsChanged(event2);
-		}
+		}*/
 	}
 
 	public void setDirPath(String dirPath) {
