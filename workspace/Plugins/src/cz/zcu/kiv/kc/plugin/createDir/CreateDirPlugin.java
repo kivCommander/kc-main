cc9dce8d08d3a6accc993797a6420257ee576a41
michalriha <michal.riha@hotmail.com>
2013-05-07 16:16:31 +0200

    fixed: https://github.com/kivCommander/kc-main/issues/21

--- workspace/Plugins/src/cz/zcu/kiv/kc/plugin/createDir/CreateDirPlugin.java
+++ workspace/Plugins/src/cz/zcu/kiv/kc/plugin/createDir/CreateDirPlugin.java
@@ -3,6 +3,8 @@ package cz.zcu.kiv.kc.plugin.createDir;
 import java.io.File;
 import java.util.List;
 
+import javax.swing.JOptionPane;
+
 import cz.zcu.kiv.kc.plugin.AbstractPlugin;
 
 public class CreateDirPlugin extends AbstractPlugin{
@@ -11,6 +13,27 @@ public class CreateDirPlugin extends AbstractPlugin{
 	public void executeAction(List<File> selectedFiles, String destinationPath,
 			String sourcePath) {
 		String name = askForName("New directory name:");
+		if (name == null)
+		{
+			JOptionPane.showMessageDialog(
+				this.mainWindow,
+				"Operace byla zrušena uživatelem.",
+				"Operace zrušena.",
+				JOptionPane.ERROR_MESSAGE
+			);
+			return;
+		}
+		if (name.trim().isEmpty())
+		{
+			JOptionPane.showMessageDialog(
+				this.mainWindow,
+				"Neplatné zadání.",
+				"Chyba.",
+				JOptionPane.ERROR_MESSAGE
+			);
+			return;
+		}
+		
 		new File(sourcePath + File.separator + name).mkdir();
 		sendEvent(sourcePath);		
 	}
